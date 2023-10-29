82e51e7c2a98b1cc2de7ca49aa53719b921b2195
Alex Randles <alexrandles0@gmail.com>
2023-10-29 01:01:12 +0000

    added hover text

--- main.py
+++ main.py
@@ -149,7 +149,7 @@ def load_rdflib_graph(rdf_string, raw_rdf_data=False, file_format="ttl"):
         return graph
     except Exception as e:
         print(f"Exception loading graph: {e}")
-        return None
+        return str(e)
 
 
 @app.route('/execute-shacl-shape', methods=["GET", "POST"])
@@ -162,43 +162,46 @@ def execute_shacl_shape():
         shacl_graph_text = form_data.get("shacl-graph-text")
         if data_graph_text:
             data_graph = load_rdflib_graph(data_graph_text, raw_rdf_data=True)
-            if not data_graph:
-                return {"error_message": "Data Graph could not be parsed!"}
+            if isinstance(data_graph, str):
+                return {"error_message": data_graph, "error_banner": "Data Graph has Incorrect Syntax!"}
         if shacl_graph_text:
             shacl_graph = load_rdflib_graph(shacl_graph_text, raw_rdf_data=True)
-            if not shacl_graph:
-                return {"error_message": "SHACL Graph could not be parsed!"}
+            if isinstance(shacl_graph, str):
+                return {"error_message": shacl_graph, "error_banner": "SHACL Graph has Incorrect Syntax!"}
 
         data_graph_file = request.files.get('data-graph-file')
-        if data_graph_file != "undefined":
-            print(data_graph_file)
-            # if data_graph_file.filename not in [".xml", ".ttl", ".json"]:
-            #     return {"error_message": "Data Graph File must be Turtle (.ttl)"}
+        if data_graph_file != "undefined" and data_graph_file:
+            # print(data_graph_file)
+            # if not data_graph_file.filename.endswith((".xml", ".ttl", ".json")):
+                # return {"error_message": "Data Graph File must be Turtle (.ttl)", "error_banner": "Incorrect File Type!"}
             # if ".owl" in data_graph_file.filename or ".xml" in data_graph_file.filename:
             #     file_format = "xml"
             file_format = rdflib.util.guess_format(data_graph_file.filename)
+            file_format = "ttl"
             data_graph = load_rdflib_graph(data_graph_file.read().decode("utf-8"), raw_rdf_data=True, file_format=file_format)
             if not data_graph:
                 return {"error_message": "Data Graph could not be parsed!"}
         shacl_graph_file = request.files.get('shacl-graph-file')
-        if shacl_graph_file:
-            if not shacl_graph_file.filename.endswith(".ttl"):
-                return {"error_message": "SHACL Graph File must be Turtle (.ttl)"}
+        if shacl_graph_file != "undefined" and shacl_graph_file:
+            # if not shacl_graph_file.filename.endswith(".ttl"):
+            #     return {"error_message": "SHACL Graph File must be Turtle (.ttl)"}
             shacl_graph = load_rdflib_graph(shacl_graph_file.read().decode("utf-8"), raw_rdf_data=True)
-            if not data_graph:
-                return {"error_message": "SHACL Graph could not be parsed!"}
+            if isinstance(shacl_graph, str):
+                return {"error_message": shacl_graph, "error_banner": "SHACL Graph has Incorrect Syntax!"}
         # exit()
 
         data_graph_uri = form_data.get("data-graph-uri")
         shacl_graph_uri = form_data.get("shacl-graph-uri")
         if data_graph_uri:
             data_graph = load_rdflib_graph(data_graph_uri)
+            print(data_graph_uri)
             if not data_graph:
                 return {"error_message": "Data Graph could not be parsed!"}
         if shacl_graph_uri:
             shacl_graph = load_rdflib_graph(shacl_graph_uri)
-            if not shacl_graph:
-                return {"error_message": "SHACL Graph could not be parsed!"}
+            if isinstance(shacl_graph, str):
+                return {"error_message": shacl_graph, "error_banner": "SHACL Graph has Incorrect Syntax!"}
+
 
 
 
